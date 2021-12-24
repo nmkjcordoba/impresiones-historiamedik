642e40f131552c96ed08edef071464c813390854
nmkjcordoba <50220781+nmkjcordoba@users.noreply.github.com>
2021-12-24 08:56:36 -0500

    cambio de fuente de letra

--- plantillas/index.js
+++ plantillas/index.js
@@ -216,7 +216,7 @@ module.exports = {
                     body {
                         padding: 1rem;
                         box-sizing:border-box;
-                        font-size: 8pt;
+                        font-size: 16px;
                         font-family: Arial, sans-serif;
                     }
                         
